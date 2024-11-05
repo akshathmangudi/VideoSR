cb906c15074ca6e3473ef3440abd4a0716a4727e
Akshath <akshathmangudi@gmail.com>
2024-11-06 01:26:21 +0530

    LPIPS almost complete

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -3,10 +3,8 @@ import cv2
 import os
 import sys
 import pandas as pd
-import torch.nn as nn
-import torch
-from LPIPS import util
-import LPIPS.models.dist as dm
+from LPIPSmodels import util
+import LPIPSmodels.dist_model as dm
 from skimage.metrics import structural_similarity as compare_ssim
 
 from absl import flags
@@ -21,13 +19,6 @@ if (not os.path.exists(FLAGS.output)):
     os.mkdir(FLAGS.output)
 
 # The operation used to print out the configuration
-registered_module = [
-    nn.Conv2d,
-    nn.ConvTranspose2d
-]
-
-# initialize
-registered_hooks, model_info_lst = [], []
 
 
 def print_configuration_op(FLAGS):
@@ -36,23 +27,6 @@ def print_configuration_op(FLAGS):
         print('\t%s: %s' % (name, str(value)))
     print('End of configuration')
 # custom Logger to write Log to file
-
-
-def hook_fn_forward(module, input, output):
-    batch_size, _, out_h, out_w = output.size()
-    gflops = batch_size * calc_gflops_per_batch(module, out_h, out_w)
-    model_info_lst.append({'gflops': gflops})
-
-
-def calc_gflops_per_batch(module, out_h, out_w):
-    """ Calculate flops of conv weights (support groups_conv & dilated_conv)
-    """
-    gflops = 0
-    if hasattr(module, 'weight'):
-        # Note: in_c is already divided by groups while out_c is not
-        out_c, in_c, k_h, k_w = module.weight.shape
-        gflops += 2*in_c*k_h*k_w*out_c*out_h*out_w/1e9
-    return gflops
 
 
 def listPNGinDir(dirpath):
@@ -126,40 +100,6 @@ def crop_8x8(img):
     x = (ori_w - w) // 2
     crop_img = img[y:y+h, x:x+w]
     return crop_img, y, x
-
-
-def register_hook(module):
-    if isinstance(module, tuple(registered_module)):
-        registered_hooks.append(module.register_forward_hook(hook_fn_forward))
-
-
-def register(model, dummy_input_dict):
-    # reset params
-    global registered_hooks, model_info_lst
-    registered_hooks, model_info_lst = [], []
-
-    # register hook
-    model.apply(register_hook)
-
-    # forward
-    _ = model(**dummy_input_dict)
-
-    # remove hooks
-    for hook in registered_hooks:
-        hook.remove()
-
-
-def profile_model(model):
-    tot_gflops = 0
-    for module_info in model_info_lst:
-        if module_info['gflops']:
-            tot_gflops += module_info['gflops']
-
-    tot_params = 0
-    for param in model.parameters():
-        tot_params += torch.prod(torch.tensor(param.size())).item()
-
-    return tot_gflops, tot_params
 
 
 class Logger(object):
